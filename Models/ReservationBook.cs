f92494023f90a5d75e64c0c4e68ceb218c9e4f6d
codingjamee <codingjamee@gmail.com>
2025-03-22 15:34:48 +0900

    ReservationBook : 1. 예약 목록을 가지고 있음 2. 키는 RoomId, 값은 List<Reservation>

---
+++ Models/ReservationBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom2.Models
+{
+    public class ReservationBook
+    {
+        private readonly Dictionary<RoomId, List<Reservation>> _roomsToReservations;
+
+        public ReservationBook()
+        {
+            _roomsToReservations = new Dictionary<RoomId, List<Reservation>>();
+        }
+    }
+}
