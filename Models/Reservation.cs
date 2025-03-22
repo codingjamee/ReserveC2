035c5d2a73407e6f5f597e94433ee97e84964214
codingjamee <codingjamee@gmail.com>
2025-03-22 15:33:18 +0900

    Reservation Class: 1. RoomId와 StartDate EndDate를 가지고 있음 2. 초기화시 생성 되며 변경할 수 없음

---
+++ Models/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom2.Models
+{
+    public class Reservation
+    {
+        public RoomId RoomId
+        {
+            get;
+        }
+        public DateTime StartDate
+        {
+            get;
+        }
+        public DateTime EndDate
+        {
+            get;
+        }
+    }
+}
