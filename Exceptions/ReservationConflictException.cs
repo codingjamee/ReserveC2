35d7193cc2d5272f13c45b29889accce6dd0d7f1
codingjamee <codingjamee@gmail.com>
2025-03-22 16:13:42 +0900

    ReservationConflictException: 클래스 초기 생성 모형

---
+++ Exceptions/ReservationConflictException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom2.Exceptions
+{
+    public class ReservationConflictException : Exception
+    {
+        public ReservationConflictException()
+        {
+        }
+
+        public ReservationConflictException(string message) : base(message)
+        {
+        }
+
+        public ReservationConflictException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ReservationConflictException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- Models/ReservationBook.cs
+++ Models/ReservationBook.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reservoom2.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
