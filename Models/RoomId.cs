5ed8c7de8c906ef6fb19e0cdf3e909a04bd63930
codingjamee <codingjamee@gmail.com>
2025-03-22 15:39:24 +0900

    RoomId Class: 1. FloorNumber 프로퍼티 2. RoomNumber 프로퍼티 3. Equals메서드 오버라이드 - roomId.FloorNumber && roomId.RoomNumber가 모두 동일 할 때

---
+++ Models/RoomId.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom2.Models
+{
+    public class RoomId
+    {
+        public int FloorNumber
+        {
+            get;
+        }
+        public int RoomNumber
+        {
+            get;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is RoomId roomId &&
+                FloorNumber == roomId.FloorNumber &&
+                RoomNumber == roomId.RoomNumber;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17; //소수
+                hash = hash * 23 + FloorNumber.GetHashCode(); //소수(17) * 소수(23)
+                hash = hash * 23 + RoomNumber.GetHashCode();
+                return hash;
+            }
+        }
+    }
+}
