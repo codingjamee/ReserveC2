2bffbf1071c63ce1d709fb2a2c8cfc2a1f2387f9
codingjamee <codingjamee@gmail.com>
2025-03-22 15:31:50 +0900

    Hotel.cs : 1. 특정 네임을 가지고 있음 (호텔네임) (초기화시 생성) 2. ReservationBook (Reservation 목록)을 참조하고 있음

---
+++ Models/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom2.Models
+{
+    public class Hotel
+    {
+        private string Name {
+            get;
+        }
+        private readonly ReservationBook _reservationBook;
+
+        public Hotel(string name)
+        {
+            Name = name;
+            _reservationBook = new ReservationBook();
+
+        }
+    }
+}
