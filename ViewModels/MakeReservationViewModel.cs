3c469bc3c3b0f7b65a89e731f6cac23c81c47e9f
codingjamee <codingjamee@gmail.com>
2025-03-24 21:44:20 +0900

    각 속성이 변할 때마다 OnpropertyChanged를 호출 - ViewModelBase를 상속받음으로써 가능 - 양방향 -> xaml의 속성 중 UpdateSourceTrigger=PropertyChanged속성을 설정함으로써

--- ViewModels/MakeReservationViewModel.cs
+++ ViewModels/MakeReservationViewModel.cs
@@ -8,6 +8,46 @@ namespace Reservoom2.ViewModels
 {
     public class MakeReservationViewModel:ViewModelBase
     {
-
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnpropertyChanged(nameof(Username));
+            }
+        } 
+        
+        private string _floorNumber;
+        public string FloorNumber
+        {
+            get
+            {
+                return _floorNumber;
+            }
+            set
+            {
+                _floorNumber = value;
+                OnpropertyChanged(nameof(FloorNumber));
+            }
+        } 
+        
+        private string _roomNumber;
+        public string RoomNumber
+        {
+            get
+            {
+                return _roomNumber;
+            }
+            set
+            {
+                _roomNumber = value;
+                OnpropertyChanged(nameof(RoomNumber));
+            }
+        }
     }
 }
